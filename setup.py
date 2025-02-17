0120db7aa997691cd852e465ff730b3a42b88067
Cem Sirin <sirincem1@gmail.com>
2025-02-17 13:55:03 +0100

    Moduling fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlx-ocr",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["mlx>=0.0.5", "numpy>=1.21.0", "pillow>=8.0.0"],
     author="Cem Sirin",
